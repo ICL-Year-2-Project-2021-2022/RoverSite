4bd5a21c339e5ca2813cfe36804d733e3366fb8e
Marco Chan <marco.chan616@gmail.com>
2022-05-30 03:45:02 -0700

    server code updated

--- rover_server/webserver.js
+++ rover_server/webserver.js
@@ -2,6 +2,8 @@ var express = require('express');
 var server = express();
 var bodyParser= require('body-parser');
 var htmlParser = require('node-html-parser');
+//nedb is a subset of mongodb, very lightweight
+var Datastore = require('nedb');
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended:true}));
 
@@ -18,8 +20,27 @@ server.get('/controller/get/json', function(req,res){
     res.end(strJSON);
 });
 
+const database = new Datastore('data_controller.db');
+database.loadDatabase();
+//load the existing database into memory. If it isn't such a database, it will create a new one
+
+
 server.post('/controller/post/json', function(req,res){
-    
+    console.log('Post Request Recieved')
+    console.log(req.body);
+    const data = req.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    res.json({
+        'status': 'success',
+        'id': data.id,
+        'countfor': data.countfor,
+        'countback': data.countback,
+        'countleft': data.countleft,
+        'countright': data.countright,
+        'timestamp': timestamp
+    });
 })
 
 // server.post('controller/post/json', function(req,res){
