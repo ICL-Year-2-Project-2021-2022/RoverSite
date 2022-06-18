c134a0e83e1ecf6f5ec2a6f5d73d95157690d471
Nikhil-Narayanan <nikhilnar02@gmail.com>
2022-06-18 22:59:13 +0100

    working function to display circles

--- frontend/client/src/components/GridBoard/GridBoard.js
+++ frontend/client/src/components/GridBoard/GridBoard.js
@@ -1,33 +1,23 @@
 import React from 'react'
-import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
+import { Stage, Layer, Circle } from 'react-konva';
 
 
-export default function GridBoard() {
+export default function GridBoard(map) {
+    let elements = []
+    let objects = map.map
+    for (let i = 0; i < objects.length; i++) {
+        if(objects[i]["type"] === "alien") {
+            elements.push(<Circle x={objects[i]["x"]} y={objects[i]["y"]} radius={objects[i]["rad"]} fill="green" />)
+        }
+        if(objects[i]["type"] === "obstacle") {
+            elements.push(<Circle x={objects[i]["x"]} y={objects[i]["y"]} radius={objects[i]["rad"]} fill="red" />)
+        }
+    }
     return(
-        <Stage width={1000} height={1000}>
-        <Layer>
-        <Text text="Some text on canvas" fontSize={15} />
-        <Rect
-          x={20}
-          y={50}
-          width={100}
-          height={100}
-          fill="red"
-          shadowBlur={10}
-        />
-        <Circle x={200} y={100} radius={50} fill="green" />
-        <Line
-          x={20}
-          y={200}
-          points={[0, 0, 100, 0, 100, 100]}
-          tension={0.5}
-          closed
-          stroke="black"
-          fillLinearGradientStartPoint={{ x: -50, y: -50 }}
-          fillLinearGradientEndPoint={{ x: 50, y: 50 }}
-          fillLinearGradientColorStops={[0, 'red', 1, 'yellow']}
-        />
-      </Layer>
+        <Stage width={1000} height={1000} strokeWidth = {2} stroke="black">
+            <Layer>
+            {elements}
+            </Layer>
       </Stage>
     )
 }
