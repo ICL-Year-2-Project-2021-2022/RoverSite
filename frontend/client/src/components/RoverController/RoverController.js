b9ffe7f55bc637b6044500b581e3fba61a053363
Nikhil-Narayanan <nikhilnar02@gmail.com>
2022-06-21 23:32:50 +0100

    Revert "Merge branch 'main' into canvas"
    This reverts commit a5f0cb4d24181ce90d6f9a69f47e1ebfbda1c0c7, reversing
    changes made to 34d42f51fcbf1c55978314752899f3bba5375716.

--- frontend/client/src/components/RoverController/RoverController.js
+++ frontend/client/src/components/RoverController/RoverController.js
@@ -10,10 +10,8 @@ export default function RoverController(props) {
     const [driveMode, setDriveMode] = useState("Control");
 
     const handleSend = event => {
-        const rotateLeftRad = Math.PI * rotateLeft / 180;
-        const rotateRightRad = Math.PI * rotateRight / 180;
         const command = {
-            driveMode, moveForward, moveBack, rotateLeft: rotateLeftRad, rotateRight: rotateRightRad
+            driveMode, rotateLeft, rotateRight, moveForward, moveBack
         };
         const URL = 'http://localhost:5000/controller/command';
         axios.post(URL, command).then(console.log);
@@ -24,28 +22,26 @@ export default function RoverController(props) {
         <div>
             <label>Drive mode</label>
             <div onChange={event => setDriveMode(event.target.value)}>
-                <input type="radio" value="Control" name="drive-mode"
-                       defaultChecked={driveMode === "Control"}/> Controlled
-                <input type="radio" value="Autonomous" name="drive-mode"
-                       defaultChecked={driveMode === "Autonomous"}/> Autonomous
+                <input type="radio" value="Control" name="drive-mode" defaultChecked={driveMode === "Control"}/> Controlled
+                <input type="radio" value="Autonomous" name="drive-mode" defaultChecked={driveMode === "Autonomous"}/> Autonomous
             </div>
             <div>
-                <label htmlFor="rotate-left">Rotate left [deg]:</label>
+                <label htmlFor="rotate-left">Rotate left:</label>
                 <input id="rotate-left" type="number" value={rotateLeft}
                        onChange={event => setRotateLeft(event.target.value)}/>
             </div>
             <div>
-                <label htmlFor="rotate-right">Rotate right [deg]:</label>
+                <label htmlFor="rotate-right">Rotate right:</label>
                 <input id="rotate-right" type="number" value={rotateRight}
                        onChange={event => setRotateRight(event.target.value)}/>
             </div>
             <div>
-                <label htmlFor="move-forward">Move forward [mm]:</label>
+                <label htmlFor="move-forward">Move forward:</label>
                 <input id="move-forward" type="number" value={moveForward}
                        onChange={event => setMoveForward(event.target.value)}/>
             </div>
             <div>
-                <label htmlFor="move-back">Move back [mm]:</label>
+                <label htmlFor="move-back">Move back:</label>
                 <input id="move-back" type="number" value={moveBack}
                        onChange={event => setMoveBack(event.target.value)}/>
             </div>
