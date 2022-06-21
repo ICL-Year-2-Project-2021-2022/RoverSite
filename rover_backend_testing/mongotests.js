a15fa6ba9c094431b923823bdddd90d8168470e3
Nikhil-Narayanan <nikhilnar02@gmail.com>
2022-06-21 16:38:02 +0100

    finally sorted dependencies :'(

--- rover_backend_testing/mongotests.js
+++ rover_backend_testing/mongotests.js
@@ -3,14 +3,12 @@ const { MongoClient } = require('mongodb');
 // import { MongoClient } from 'mongodb'
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
-console.log("hey")
+const url = 'mongodb://127.0.0.1:27017';
 
 const client = new MongoClient(url);
 
 // Database Name
 const dbName = 'myProject';
-console.log("hey")
 
 async function main() {
   // Use connect method to connect to the server
