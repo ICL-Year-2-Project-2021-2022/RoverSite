b15d8d17aa9041595394671d43d141d743bf800f
Nikhil-Narayanan <nikhilnar02@gmail.com>
2022-06-21 21:58:14 +0100

    restructured backend

--- rover_backend_testing/webserver.js
+++ rover_backend_testing/webserver.js
@@ -2,31 +2,92 @@ const express = require('express');
 const server = express();
 const bodyParser = require('body-parser');
 //nedb is a subset of mongodb, very lightweight
-const Datastore = require('nedb');
 const cors = require('cors');
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(cors({origin: '*'}));
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
 
-const commandsDatabase = new Datastore('./data/commands.db');
-commandsDatabase.loadDatabase();
-commandsDatabase.remove({}, {multi: true}, (err, numRemoved) => console.error('problem clearing DB: ' + err));
-let commandOrder = 0;
+// Connection URL
+const url = 'mongodb://127.0.0.1:27017';
 
-server.post('/controller/command', (req, res) => {
-    console.log('/controller/command');
-    const command = req.body;
-    command.order = commandOrder;
-    commandOrder++;
-    commandsDatabase.insert(command);
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({message: "Command received"}));
-});
+const client = new MongoClient(url);
 
-const database_telemetry = new Datastore('./data/telemetry.db');
-database_telemetry.loadDatabase();
-database_telemetry.remove({}, {multi: true}, (err, numRemoved) => console.error('problem clearing DB: ' + err));
-let telemetryOrder = 0;
+const tel_name = 'telemetry';
+
+const com_name = 'commands';
+
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const telemetry_db = client.db(tel_name);
+    //TODO: Clear DB
+    const collection = telemetry_db.collection('documents');
+    const command_db = client.db(com_name)
+    const command_collection = command_db.collection('documents')
+
+    let commandOrder = 0;
+
+    server.post('/controller/command', (req, res) => {
+        console.log('/controller/command');
+        const command = req.body;
+        command.order = commandOrder;
+        commandOrder++;
+        await command_db.insertOne(command);
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({message: "Command received"}));
+    });
+
+    let telemetryOrder = 0;
+
+    server.post('/rover/telemetry', (req, res) => {
+        const currentDateTime = new Date();
+        console.log("/rover/telemetry message received: " + currentDateTime);
+    
+        const kalmanState = parseKalmanStateToJSON(req.body.kalmanState);
+        const kalmanVariances = parseKalmanVariancesToJSON(req.body.kalmanVariances);
+        const map = constructMapFromStateAndVariances(kalmanState, kalmanVariances, ['R', 'G']);
+    
+        const dbRecord = {
+            order: telemetryOrder,
+            map,
+            status: {
+                batteryPercentage: parseInt(req.body.batteryPercentage),
+                opticalFlowSensor1: parseInt(req.body.opticalFlowSensor1),
+                opticalFlowSensor2: parseInt(req.body.opticalFlowSensor2)
+            }
+        };
+        await telemetry_db.insertOne(dbRecord);
+        telemetryOrder++;
+    
+        const commandOrder = parseInt(req.body.commandOrder);
+
+        //TODO: Refactor below
+        commandsDatabase.find({order: {$gt: commandOrder}}).sort({order: 1}).limit(1).exec((errs, docs) => {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            if (docs[0]) {
+                res.end(JSON.stringify(docs[0]));
+            } else {
+                res.end(JSON.stringify({order: -2, message: "No command found"}));
+            }
+        });
+    });
+    
+    server.get('/controller/telemetry', (req, res) => {
+        console.log("telemetry");
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        //TODO: Refactor below
+        database_telemetry.find({}).sort({order: -1}).limit(1).exec((err, data) => {
+            res.end(JSON.stringify(data));
+        });
+    });
+
+    return 'done.';
+  }
+  
 
 
 const objectToColourMapping = {
@@ -78,44 +139,10 @@ const constructMapFromStateAndVariances = (state, variances, landmarkTypes) => {
     return map;
 };
 
-server.post('/rover/telemetry', (req, res) => {
-    const currentDateTime = new Date();
-    console.log("/rover/telemetry message received: " + currentDateTime);
-
-    const kalmanState = parseKalmanStateToJSON(req.body.kalmanState);
-    const kalmanVariances = parseKalmanVariancesToJSON(req.body.kalmanVariances);
-    const map = constructMapFromStateAndVariances(kalmanState, kalmanVariances, ['R', 'G']);
-
-    const dbRecord = {
-        order: telemetryOrder,
-        map,
-        status: {
-            batteryPercentage: parseInt(req.body.batteryPercentage),
-            opticalFlowSensor1: parseInt(req.body.opticalFlowSensor1),
-            opticalFlowSensor2: parseInt(req.body.opticalFlowSensor2)
-        }
-    };
-    database_telemetry.insert(dbRecord);
-    telemetryOrder++;
-
-    const commandOrder = parseInt(req.body.commandOrder);
-    commandsDatabase.find({order: {$gt: commandOrder}}).sort({order: 1}).limit(1).exec((errs, docs) => {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        if (docs[0]) {
-            res.end(JSON.stringify(docs[0]));
-        } else {
-            res.end(JSON.stringify({order: -2, message: "No command found"}));
-        }
-    });
-});
-
-server.get('/controller/telemetry', (req, res) => {
-    console.log("telemetry");
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    database_telemetry.find({}).sort({order: -1}).limit(1).exec((err, data) => {
-        res.end(JSON.stringify(data));
-    });
-});
-
 server.listen(5000, "0.0.0.0");
 console.log("Listening on port " + 5000);
+
+main()
+.then(console.log)
+.catch(console.error)
+.finally(() => client.close());
