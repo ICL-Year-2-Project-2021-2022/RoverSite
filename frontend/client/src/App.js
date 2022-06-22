2100db8dd8ad4a3ceb94d5b53e7744e7ed05fd5e
Nikhil-Narayanan <nikhilnar02@gmail.com>
2022-06-22 13:43:39 +0100

    used state hook for sum

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -21,12 +21,12 @@ const dummyTelemetryData = {
 
 function App() {
     const [telemetry, setTelemetry] = useState({map: [], status: {}});
-    let sum = 0
-    let len = 0
+    const [sum, setSum] = useState(0)
+    const [len, setLen] = useState(0)
     let rtt = 0
     useEffect(() => {
         const inteval = setInterval(() => {
-            const URL = "http://localhost:5000/controller/telemetry";
+            const URL = "http://172.16.207.255:5000/controller/telemetry";
             let d = new Date()
             axios.get(URL)
                 .then(res => {
@@ -37,8 +37,8 @@ function App() {
                 })
                 .catch(err => console.log(err));
             let b = new Date()
-            sum = sum + b.getTime() - d.getTime()
-            len = len + 1
+            setSum(b.getTime() - d.getTime())
+            setLen(len + 1)
             rtt = sum/len
             console.log(len)
             console.log(rtt)
