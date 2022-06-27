657cb84bccc1ea43c39dabf30e89df5e9dcf33f9
TimothyJNewman <84350177+TimothyJNewman@users.noreply.github.com>
2022-06-27 22:02:26 +0100

    Centered telemetry

--- frontend/client/src/components/Telemetry/Telemetry.js
+++ frontend/client/src/components/Telemetry/Telemetry.js
@@ -2,39 +2,39 @@ import React from 'react'
 import './Telemetry.css'
 
 export default function Telemetry(props) {
-    
-    return (
-        <div>
-            <h3 className="score-title">Telemetry</h3>
-        <table>
-  <thead>
-    <tr>
-      <th>Attribute</th>
-      <th>Value</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Current (mA)</td>
-      <td>{props.averageCurrent}</td>
-    </tr>
-    <tr>
-      <td>Battery (%)</td>
-      <td>{props.batteryPercentage}</td>
-    </tr>
-    <tr>
-      <td>Charge (mAh)</td>
-      <td>{props.batteryRemaining} </td>
-    </tr>
-    <tr>
-      <td>S-Qual 1 </td>
-      <td>{props.opticalFlowSensor1}</td>
-    </tr>
-    <tr>
-      <td>S-Qual 2</td>
-      <td>{props.opticalFlowSensor2}</td>
-    </tr>
-  </tbody>
-</table></div>
-    )
+
+  return (
+    <div>
+      <h3 className="score-title">Telemetry</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Attribute</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Current (mA)</td>
+            <td>{props.averageCurrent}</td>
+          </tr>
+          <tr>
+            <td>Battery (%)</td>
+            <td>{props.batteryPercentage}</td>
+          </tr>
+          <tr>
+            <td>Charge (mAh)</td>
+            <td>{props.batteryRemaining} </td>
+          </tr>
+          <tr>
+            <td>S-Qual 1 </td>
+            <td>{props.opticalFlowSensor1}</td>
+          </tr>
+          <tr>
+            <td>S-Qual 2</td>
+            <td>{props.opticalFlowSensor2}</td>
+          </tr>
+        </tbody>
+      </table></div>
+  )
 }
